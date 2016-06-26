9e34629c179f5c5e0f15c9e18378c9be49c591a8
Juarez ASF <juarez.asf@gmail.com>
2016-06-26 19:35:03 -0300

    adding comments to this FaceTracker

--- FaceTracker/main.cpp
+++ FaceTracker/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main() {
 
+    //loads tracker and its parameters
     FACETRACKER::FaceTracker *tracker = FACETRACKER::LoadFaceTracker();
     if (tracker == nullptr) {
         cerr << "Cannot load tracker!" << endl;
@@ -28,31 +29,36 @@ int main() {
     }
 
     bool shouldFinish = false;
+
+    //frame is used for web cam input; but the CSIRO face detection works only on gray scale images
     cv::Mat frame, grayScale;
+    int detectionQuality;
 
     cout << "press 'q' to quit" << endl;
-    while(!shouldFinish){
+    while (!shouldFinish) {
         cam >> frame;
-
 
         cv::cvtColor(frame, grayScale, cv::COLOR_BGR2GRAY);
 
-        int detectionQuality = tracker->NewFrame(grayScale, params);
+        //update state of tracker with new frame
+        detectionQuality = tracker->NewFrame(grayScale, params);
 
-        if(detectionQuality == 0 || detectionQuality == FACETRACKER::FaceTracker::TRACKER_FAILED){
+        if (detectionQuality == 0 || detectionQuality == FACETRACKER::FaceTracker::TRACKER_FAILED) {
             tracker->Reset();
         }
 
         cout << "Detection quality is: " << detectionQuality << endl;
 
+        //obtain points that were tracked
         auto points = tracker->getShape();
 
-        for(auto p : points){
-            cv::circle(frame, p, 2, cv::Scalar(255,0,0), 2);
+        //draw point on input frame
+        for (auto p : points) {
+            cv::circle(frame, p, 2, cv::Scalar(255, 0, 0), 2);
         }
         cv::imshow("input", frame);
 
-        if(char q = (char)(cv::waitKey(30) & 0xFF) == 'q'){
+        if (char q = (char) (cv::waitKey(30) & 0xFF) == 'q') {
             shouldFinish = true;
         }
 
